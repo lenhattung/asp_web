dc9542dda3b3d658040beaf097a452f379c32f84
Le Nhat Tung <lenhattung@gmail.com>
2024-09-06 13:00:05 +0700

    update

--- BaiTap03/obj/Debug/net8.0/BaiTap03.AssemblyInfo.cs
+++ BaiTap03/obj/Debug/net8.0/BaiTap03.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTap03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b881e22c46475ab25e85c13d31bf7612194910f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cb0a343b31af915490d428bf911ceb207c47d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTap03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTap03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
